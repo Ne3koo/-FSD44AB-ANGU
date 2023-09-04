08a85e0c581ed2c137e942987aa3bafe0176cd66
Guillaume Boit <guillaume.boit@free.fr>
2023-09-04 12:24:21 +0200

    duck

---
+++ SEMAINE_01/J1_2_intro_typescript/Exercices/chap2/Duck/app.ts
@@ -0,0 +1,21 @@
+// définition de l'interface <=> contrat
+interface Duck{
+    name : string ;
+    swim(): string; 
+}
+
+interface Comp {
+    age: number;
+}
+
+class Thing implements Duck, Comp {
+    name: string;    
+    age: number;
+    
+    swim(): string {
+       return "Nage comme un canard";
+    }
+}
+
+let duck = new Thing;
+console.log(duck.swim());
