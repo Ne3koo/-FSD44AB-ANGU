c1971af022097f10006fe28934b5faeb36cdd754
Guillaume Boit <guillaume.boit@free.fr>
2023-09-04 14:00:54 +0200

    TP

--- SEMAINE_01/J1_2_intro_typescript/Exercices/chap3/Queue/app.ts
+++ SEMAINE_01/J1_2_intro_typescript/Exercices/chap3/Queue/app.ts
@@ -4,12 +4,14 @@ import { numbers, listNumbers } from "./Data/data";
 
 const queue = new Queue<number>();
 
-for(const number of numbers) queue.push(number);
+for(const value of numbers) queue.push(value);
 
-console.log(queue.pop()); // affiche la dernière valeur ajouter dans la queue
+console.log(queue.pop()); // affiche la première valeur ajoutée dans la queue
+
+
 
 const queueList = new Queue<number[]>();
 
 for(const number of listNumbers) queueList.push(number);
 
-console.log(queueList.pop()); // affiche la dernière valeur ajouter dans la queue
+console.log(queueList.pop()); // affiche la première valeur ajoutée dans la queue
